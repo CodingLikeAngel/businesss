b5059eaf878045f22947331f2c1f8f246d582157
angel <angel.nieto@babooni.com>
2026-01-18 05:11:29 +0100

    kilo code es una mierda, la siguiente tarea seria definir un componente que sea un div generico contenedor donde arrastrar componentes y asi poder crear un grid

--- libs/ui-components/src/lib/newsletter-section/newsletter-section.component.ts
+++ libs/ui-components/src/lib/newsletter-section/newsletter-section.component.ts
@@ -1,19 +1,9 @@
 
-import { Component, input, output, computed } from '@angular/core';
+import { Component, input, output, ViewEncapsulation, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UIInputComponent } from '../forms/input/input.component';
 import { UIButtonComponent } from '../button/button.component';
-
-export interface NewsletterCustomStyles {
-  backgroundColor?: string;
-  color?: string;
-  '--theme-bg'?: string;
-  '--theme-color'?: string;
-  '--component-bg'?: string;
-  '--component-text'?: string;
-  [key: string]: string | undefined;
-}
 
 @Component({
   selector: 'lib-ui-newsletter-section',
@@ -28,49 +18,6 @@ export class UINewsletterSectionComponent {
   placeholder = input('Tu correo electrónico');
   buttonText = input('Suscribirse');
   variant = input('primary');
-  customStyles = input<NewsletterCustomStyles>({});
-
-  newsletterStyles = computed(() => {
-    const styles: Record<string, any> = {};
-    const customStyles = this.customStyles();
-     
-    if (customStyles['backgroundColor']) {
-      styles['--theme-bg'] = customStyles['backgroundColor'];
-      styles['--component-bg'] = customStyles['backgroundColor'];
-      styles['background'] = customStyles['backgroundColor'];
-      styles['background-color'] = customStyles['backgroundColor'];
-    }
-     
-    if (customStyles['color']) {
-      styles['--theme-color'] = customStyles['color'];
-      styles['--component-text'] = customStyles['color'];
-      styles['color'] = customStyles['color'];
-    }
-     
-    if (customStyles['--theme-bg']) {
-      styles['--theme-bg'] = customStyles['--theme-bg'];
-    }
-     
-    if (customStyles['--theme-color']) {
-      styles['--theme-color'] = customStyles['--theme-color'];
-    }
-     
-    if (customStyles['--component-bg']) {
-      styles['--component-bg'] = customStyles['--component-bg'];
-    }
-     
-    if (customStyles['--component-text']) {
-      styles['--component-text'] = customStyles['--component-text'];
-    }
-     
-    Object.keys(customStyles).forEach(key => {
-      if (key !== 'backgroundColor' && key !== 'color' && key !== '--theme-bg' && key !== '--theme-color' && key !== '--component-bg' && key !== '--component-text') {
-        styles[key] = customStyles[key];
-      }
-    });
-     
-    return styles;
-  });
   
   subscribe = output<string>();
 
